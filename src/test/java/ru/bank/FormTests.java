ca4dd76a9e5786f574eeb952ab7a65ceb0405a34
Danil Zhikharev <dane99@mail.ru>
2022-12-25 19:58:20 +0500

    Commit_25.12_19:58

---
+++ src/test/java/ru/bank/FormTests.java
@@ -0,0 +1,32 @@
+package ru.bank;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormTests {
+
+  String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+  @BeforeEach
+  void settings() {
+    //        Configuration.headless = true;
+    open("http://localhost:9999/");
+  }
+
+  @Test
+  void happyPath() {
+    $x("//*[@data-test-id='city']//child::*[@placeholder='Город']").setValue("Москва");
+    $x("//*[@placeholder='Дата встречи']").setValue(String.valueOf(date));
+    $("[name=name]").setValue("Иванов Иван");
+    $("[name=phone]").setValue("+79126178980");
+    $x("//*[@data-test-id='agreement']").click();
+    $x("//*[contains(text(), 'Забронировать')]").click();
+    $(".notification__content").should(appear, Duration.ofMillis(15000)).shouldHave(text("Встреча успешно забронирована на " + date));
+  }
+}
